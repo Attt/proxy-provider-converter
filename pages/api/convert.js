a415fc5439c6b3529aeb29f7fcb0e9bee0f2c987
atpexgo.wu <atpexgo.wu@ximalaya.com>
2024-05-16 17:33:40 +0800

    Support cloudflare pages

--- pages/api/convert.js
+++ pages/api/convert.js
@@ -1,6 +1,11 @@
 const YAML = require("yaml");
 const axios = require("axios");
 
+export const runtime = 'edge';
+
+export const config = {
+	runtime: 'edge',
+};
 
 export default async (req, res) => {
   const url = req.query.url;
@@ -122,4 +127,3 @@ export default async (req, res) => {
   }
 };
 
-export const runtime = 'edge';
