f0463535f024d7c637ac425a1a3a364cd9c4c7b9
atpexgo.wu <atpexgo.wu@ximalaya.com>
2024-05-16 17:44:20 +0800

    Support cloudflare pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,21 @@ import "tailwindcss/tailwind.css";
 import "../styles/global.css";
 import splitbee from "@splitbee/web";
 
+export const config = {
+	runtime: 'edge',
+};
+
+export const getServerSideProps = async () => {
+	return {
+		props: {
+			runtime: process.env.NEXT_RUNTIME,
+			uuid: await fetch('https://uuid.rocks/plain').then(response =>
+				response.text(),
+			),
+		},
+	};
+};
+
 function MyApp({ Component, pageProps }) {
   splitbee.init({
     scriptUrl: "/bee.js",
@@ -9,5 +24,4 @@ function MyApp({ Component, pageProps }) {
   });
   return <Component {...pageProps} />;
 }
-export const runtime = 'edge';
 export default MyApp;
