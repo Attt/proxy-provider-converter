c388c7db84101bacde0b91196b9253498c581016
Attt <atpexgo@gmail.com>
2024-05-17 13:10:46 +0800

    Change logo

--- app/page.tsx
+++ app/page.tsx
@@ -268,12 +268,12 @@ ${urlHost || "egroup"} = select, policy-path=${convertedUrl}
       <footer className="w-full p-4 max-w-4xl md:py-8">
         <a
           className="flex items-center"
-          href="https://vercel.com"
+          href="https://vercel.com/frameworks/nextjs"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          <img src="/next.svg" alt="Next Logo" className="h-4 ml-2" />
         </a>
       </footer>
 
